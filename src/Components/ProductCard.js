fb462cdb5f9007de25ba8659f533a4e1eabe5162
Jidmaa <majidbenhenneda@gmail.com>
2021-07-03 22:16:28 +0100

    add to cart button, hovers and more !

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Context from "./Context";
 import { getCurrencySymbol } from "../Functions/util";
-
+import cart from "../addtocart.svg";
 class ProductCard extends React.Component {
   static contextType = Context;
   render() {
     const { product } = this.props;
     const { selectedCurrency } = this.context;
     return (
-      <div>
+      <div className={"product-card " + (!product.inStock && "out-of-stock")}>
         <img
           width={330}
           height={354}
@@ -16,8 +16,12 @@ class ProductCard extends React.Component {
           alt={product.name}
           className="product-img"
         />
+        {!product.inStock && (
+          <div className="out-of-stock-text"> OUT OF STOCK </div>
+        )}
+        <img src={cart} alt="add_to_cart" className="add-to-cart" />
         <p> {product.name}</p>
-        <span>
+        <span className="price ">
           {getCurrencySymbol(selectedCurrency)}{" "}
           {
             product.prices.find(({ currency }) => currency == selectedCurrency)
